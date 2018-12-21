e4ef4d484b45d6c1956faf6ad3d48c7c906efc37
imckh kompakar <987992518@qq.com>
2018-12-21 15:50:18 +0800

    242. 有效的字母异位词

---
+++ src/leetcode/explore/easy/strings/P242.java
@@ -0,0 +1,80 @@
+/*
+242. 有效的字母异位词
+https://leetcode-cn.com/problems/valid-anagram/
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/35/
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的一个字母异位词。
+
+示例 1:
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+示例 2:
+
+输入: s = "rat", t = "car"
+输出: false
+说明:
+你可以假设字符串只包含小写字母。
+
+进阶:
+如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+类似的题目: 49 字母异位词分组
+*/
+import java.util.*;
+public class P242 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isAnagram("ab", "a"));
+    }
+
+    
+    static class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s == null && t == null) {
+                return true;
+            }
+            if (s == null || t == null) {
+                return false;
+            }
+
+            char[] sc = s.toCharArray();
+            Arrays.sort(sc);
+            char[] tc = t.toCharArray();
+            Arrays.sort(tc);
+            if (sc.length != tc.length) return false;
+            for (int i = 0; i < sc.length; i++) {
+                if (sc[i] != tc[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public boolean isAnagram2(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] tem = new int[26];
+            char[] sc = s.toCharArray();
+            for (int i = 0; i < sc.length; i++) {
+                tem[sc[i] - 'a']++;
+            }
+            char[] tc = t.toCharArray();
+            for (int i = 0; i < tc.length; i++) {
+                tem[tc[i] - 'a']--;
+            }
+            int index = 0;
+            for (int i = 0; i < tem.length; i++) {
+                index = i;
+                if (tem[i] != 0) {
+                    break;
+                }
+            }
+            if (index == 25) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
