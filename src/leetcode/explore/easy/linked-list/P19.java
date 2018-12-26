f8f8f48ff75bd75a18579c0273fca9323ca287a3
imckh <987992518@qq.com>
2018-12-26 22:13:50 +0800

    19. 删除链表的倒数第N个节点

---
+++ src/leetcode/explore/easy/linked-list/P19.java
@@ -0,0 +1,149 @@
+/*
+19. 删除链表的倒数第N个节点
+https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/42/
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+当删除了倒数第二个节点后，链表变为 1->2->3->5.
+说明：
+
+给定的 n 保证是有效的。
+
+进阶：
+
+你能尝试使用一趟扫描实现吗？
+*/
+public class P19 {
+    public static void main(String[] args) {
+        ListNode head = stringToListNode("[1,2,3,4,5]");
+        ListNode head2 = stringToListNode("[1,2,3,4,5]");
+        prettyPrintLinkedList(head);
+        prettyPrintLinkedList(new Solution().removeNthFromEnd1(head, 6));
+        prettyPrintLinkedList(new Solution().removeNthFromEnd(head2, 6));
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static void prettyPrintLinkedList(ListNode node) {
+        while (node != null && node.next != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+
+        if (node != null) {
+            System.out.println(node.val);
+        } else {
+            System.out.println("Empty LinkedList");
+        }
+    }
+}
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    // 平民方法: 数一下一共有几个, 然后删掉, 遍历两边
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length  = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    // 双指针, 一个指向当前节点, 另一个指向当前节点的前n个节点
+    // 结束时将另一个节点的next删掉
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        if (n < 1) return head;
+        
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode beforeN = temp;
+        ListNode cur = temp;
+        int distance = 1;
+        while (cur.next != null) {
+            if (distance > n) {
+                beforeN = beforeN.next;
+            }
+            distance++;
+            cur = cur.next;
+        }
+
+        beforeN.next = beforeN.next.next;
+
+        return temp.next;
+    }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // 不用每次都判断一下
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
